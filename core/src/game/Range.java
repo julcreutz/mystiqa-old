9da4e4d19bda73380d6cfec964fd426340b36bb2
Julian Creutz <julcreutz@web.de>
2018-12-30 18:04:23 +0100

    Start more documentation

--- core/src/game/Range.java
+++ core/src/game/Range.java
@@ -5,23 +5,42 @@ import game.loader.Serializable;
 
 import java.util.Random;
 
+/** Class enclosing interval with two inclusive boundaries. */
 public class Range implements Serializable {
+    /** Lower bound. */
     public int min;
+
+    /** Higher bound. */
     public int max;
 
+    /** Constructs range with given boundaries. */
     public Range(int min, int max) {
         this.min = min;
         this.max = max;
     }
 
+    /** Constructs range with boundaries of approximately -infinite to infinite. */
     public Range() {
         this(-Integer.MAX_VALUE, Integer.MAX_VALUE);
     }
 
+    /**
+     * Checks whether given value is in range of bounds.
+     *
+     * @param val value to be checked
+     * @return whether value is within bounds
+     */
     public boolean inRange(int val) {
         return val >= min && val <= max;
     }
 
+    /**
+     * Picks a random value within bounds of range. Uses given
+     * {@link Random} object for random number generation.
+     *
+     * @param rand random number generator
+     * @return random value within bounds
+     */
     public int pickRandom(Random rand) {
         return min + rand.nextInt(max - min + 1);
     }
